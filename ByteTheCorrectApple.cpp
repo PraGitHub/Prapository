7dfa2899f7bdda127589d8ba0842493cfa84f71d
PraGitHub <39001752+PraGitHub@users.noreply.github.com>
2018-06-23 13:18:30 +0530

    ThisChangeFetched57Points
    ThisChangeFetched57Points

--- ByteTheCorrectApple.cpp
+++ ByteTheCorrectApple.cpp
@@ -1,9 +1,9 @@
-#include <cmath>
 #include <cstdio>
 #include <vector>
 #include <iostream>
 #include<fstream>
 #include <algorithm>
+#include<string>
 using namespace std;
 
 string strArrayCompany[]={
@@ -34,11 +34,13 @@ string strArrayFruit[]={
 int FindScore(string strLine,string* strArray){
     int iScore = 0;;
     for(int i=0;true;i++){
+        //cout<<strArray[i]<<" ";
         if(strArray[i]=="EOA"){
             break;
         }
         if(strLine.find(strArray[i])!=strLine.npos){
             iScore++;
+            //cout<<strArray[i]<<" ";
         }
     }
     return iScore;
@@ -59,7 +61,7 @@ void strToLower(string &strIn)
 }
 
 string Decide(int iScoreCompany, int iScoreFruit){
-    cout<<"iScoreCompany = "<<iScoreCompany<<";"<<"iScoreFruit = "<<iScoreFruit<<endl;
+    //cout<<"iScoreCompany = "<<iScoreCompany<<";"<<"iScoreFruit = "<<iScoreFruit<<endl;
     if(iScoreCompany>iScoreFruit){
         return "computer-company";
     }
@@ -73,16 +75,23 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     int iNumLines;
     cin>>iNumLines;
-    for(int i=0;i<iNumLines;i++){
+    for(int i=0;i<=iNumLines;i++){
+        //cout<<i<<" : ";
         string strLine;
         string strResult;
         int iScoreCompany;
         int iScoreFruit;
-        cin>>strLine;
+        getline(cin,strLine);
+        if(i==0){
+            continue;
+        }
+        //cout<<"Length Before Lower= "<<strLine.length();
         strToLower(strLine);
+        //cout<<" ; Length After Lower = "<<strLine.length();
         iScoreCompany = FindScore(strLine,strArrayCompany);
         iScoreFruit = FindScore(strLine,strArrayFruit);
         strResult = Decide(iScoreCompany,iScoreFruit);
+        //cout<<"C:"<<iScoreCompany<<";F:" <<iScoreFruit<<endl;
         cout<<strResult<<endl;
     }
     return 0;
