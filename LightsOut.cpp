22e79cab74750cb0787990643f7a685e9f5dc48a
PraGitHub <39001752+PraGitHub@users.noreply.github.com>
2018-06-28 22:36:52 +0530

    SimplerAndBetter43Points
    This fetched 43 points though the logic appears simpler.

--- LightsOut.cpp
+++ LightsOut.cpp
@@ -2,60 +2,18 @@
 #include <vector>
 using namespace std;
 
-static int iXMain = 0;
-static int iYMain = 0;
-
-void nextMove(int player, vector <string> board){
-    int iX = iXMain;
-    int iY = iYMain;
-    for(int i=iX;i<8;i++){
-        for(int j=iY;j<8;j++){
-            if(i+1<8 && j+1<8){
-                if(board[i][j]==1){
-                    iXMain = i;
-                    iYMain = j+1;
-                    if(j==7){
-                        iXMain = i+1;
-                        iYMain = 0;
-                    }
-                    cout<<i<<" "<<j<<endl;
-                    return;
-                }
-            }
-            if(j==7){
-                if(board[i][j]==1){
-                    iXMain = i+1;
-                    iYMain = 0;
-                    cout<<i<<" "<<j<<endl;
-                    return;
-                }
-            }
-            if(board[i][j]==1){
-                iXMain = i;
-                iYMain = j+1;
-                cout<<i<<" "<<j<<endl;
-                return;
-            }
+int main()
+{
+    int iPlayer;
+    cin>>iPlayer;
+    for(int i=0;i<8;i++){
+        string strLine;
+        cin>>strLine;
+        size_t iPos = -1;
+        if((iPos = strLine.find("1"))!=strLine.npos){
+            cout<<i<<" "<<iPos<<endl;
+            break;
         }
     }
-}
-
-int main(void) {
-
-    int player;
-    vector <string> board;
-
-    //If player is 1, I'm the first player.
-    //If player is 2, I'm the second player.
-    cin >> player;
-
-    //Read the board now. The board is a 8x8 array filled with 1 or 0.
-    for(int i=0; i<8; i++) {
-        string s; cin >> s;
-        board.push_back(s);
-    }
-
-    nextMove(player,board);
-
     return 0;
 }
