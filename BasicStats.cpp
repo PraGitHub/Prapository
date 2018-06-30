60fd1f404a3f92c03701ff799061a61b773693dc
PraGitHub <39001752+PraGitHub@users.noreply.github.com>
2018-06-30 12:40:16 +0530

    ConfidenceLevelIntervals

--- BasicStats.cpp
+++ BasicStats.cpp
@@ -24,8 +24,8 @@ void Swap(double &a, double &b){
 }
 
 void SortSet(double* dArraySet, int iN){
-    /*int iSize = sizeof(dArraySet)/sizeof(dArraySet[0]);
-    sort(dArraySet,dArraySet+iSize);*/
+    int iSize = sizeof(dArraySet)/sizeof(dArraySet[0]);
+    sort(dArraySet,dArraySet+iSize);
     for(int i=0;i<iN-1;i++){
         for(int j=i+1;j<iN;j++){
             if(dArraySet[i]>dArraySet[j]){
@@ -53,6 +53,35 @@ double FindMedian(double* dArraySet, int iN){
     
 }
 
+//This function will find lower and upper limit of confidence interval only for,
+// N >= 10 (degree of freedom >=9)
+// Confidence interval kind is 95% (alpha = 0.025)
+//Refer the below link for the better understanding of confidence interval : 
+//http://www.statisticshowto.com/probability-and-statistics/confidence-interval/
+void FindConfidenceInterval(double dMean, double dSD, int iN, double &dLowerLimit, double &dUpperLimit){
+    int iDegreeOfFreedom = iN-1;
+    double dAlpha = 0.025;
+    double dT = 2.0;
+    
+    if(iDegreeOfFreedom>=120){
+        dT = 1.9;
+    }
+    else if(iDegreeOfFreedom>=12 && iDegreeOfFreedom<=18){
+        dT = 2.1;
+    }
+    else if(iDegreeOfFreedom>=9 && iDegreeOfFreedom<=11){
+        dT = 2.2;
+    }
+    else;
+    
+    dT = 1.96; 
+    //I am hardcoding dT to 1.96 because in the contest they expected it to be for infinity
+    //This can be removed or kept according to the requirement
+    
+    dLowerLimit = dMean - dT*dSD/(sqrt(1.0*iN));
+    dUpperLimit = dMean + dT*dSD/(sqrt(1.0*iN));
+}
+
 void PrintWithPrecision(double dData,int iPrecision){
     cout<</*setprecision(iPrecision)<<*/dData<<endl;
 }
@@ -66,6 +95,8 @@ int main() {
     double dMedian = 0;
     double dMode = 0;
     double dSD = 0;
+    double dLowerLimit = 0;
+    double dUpperLimit = 0;
     cin>>iN;
     double dArraySet[iN];
     for(int i=0;i<iN;i++){
@@ -100,10 +131,13 @@ int main() {
     SortSet(dArraySet,iN);
     dMedian = FindMedian(dArraySet,iN);
     
+    FindConfidenceInterval(dMean, dSD, iN, dLowerLimit,dUpperLimit);
+    
     PrintWithPrecision(dMean,1);
     PrintWithPrecision(dMedian,1);
     PrintWithPrecision(dMode,1);
     PrintWithPrecision(dSD,1);
+    cout<<dLowerLimit<<" "<<dUpperLimit<<endl;
     
     return 0;
 }
