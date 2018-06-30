c59404611f092c81a3a841148f9d07a9a82e2659
PraGitHub <39001752+PraGitHub@users.noreply.github.com>
2018-06-30 18:30:50 +0530

    CreatingMaxAndMinTempArrayAndTheirXAxes

--- TemperaturePrediction.cpp
+++ TemperaturePrediction.cpp
@@ -27,12 +27,57 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
     int iN;
     cin>>iN;
+    double dArrayTempMax[iN]; // y axis
+    double dArrayTempMaxRef[iN];//x axis
+    double dArrayTempMin[iN];//y axis
+    double dArrayTempMinRef[iN];//x axis
+    int iMaxArrayLen = 0;
+    int iMinArrayLen = 0;
+    string strMissingMax = "";
+    string strMissingMin = "";
     for(int i=0;i<=iN;i++){
         for(int j=0;j<4;j++){
             if(i==0){
                 continue;
             }
             stirng strYear;
+            string strMonth;
+            string strTempMax;
+            string strTempMin;
+            bool bSkipMax = false;
+            bool bSkipMin = false;
+            cin>>strYear;
+            cin>>strMonth;
+            cin>>strTempMax;
+            cin>>strTempMin;
+            if(strTempMax.find("Missing")!=strTempMax.npos){
+                if(strMissingMax == ""){
+                    strMissingMax = strMissingMax + strTempMax + IntToString(i); 
+                }
+                else{
+                   strMissingMax = ","+strMissingMax + strTempMax + IntToString(i);   
+                }
+                bSkipMax = true;
+            }
+            if(strTempMin.find("Missing")!=strTempMin.npos){
+                if(strMissingMin == ""){
+                    strMissingMin = strMissingMin + strTempMin + IntToString(i); 
+                }
+                else{
+                    strMissingMin = ","+strMissingMin + strTempMin + IntToString(i); 
+                }
+                bSkipMin = true;
+            }
+            if(bSkipMax == false){
+                dArrayTempMax[iMaxArrayLen] = StringToDouble(strTempMax);
+                dArrayTempMaxRef[iMaxArrayLen] = i;
+                iMaxArrayLen++;
+            }
+            if(bSkipMin == false){
+                dArrayTempMin[iMinArrayLen] = StringToDouble(strTempMin);
+                dArrayTempMinRef[iMinArrayLen] = i;
+                iMinArrayLen++;
+            }
         }
     }
     return 0;
