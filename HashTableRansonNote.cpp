5a06b8a50bef17f1266c000454f5a9bbeaaffc33
PraGitHub <39001752+PraGitHub@users.noreply.github.com>
2018-07-02 15:51:43 +0530

    PlanCommit

---
+++ HashTableRansonNote.cpp
@@ -0,0 +1,7 @@
+/*
+For the problem - https://www.hackerrank.com/challenges/ctci-ransom-note/problem
+
+Create a Linked list of all magazine strings
+
+Read every ransom string and if found in list delete the node
+*/
