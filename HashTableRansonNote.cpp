1b24b653e8062f311bd1fd718fb4179d190df062
PraGitHub <39001752+PraGitHub@users.noreply.github.com>
2018-07-02 16:06:38 +0530

    CreatingDS

--- HashTableRansonNote.cpp
+++ HashTableRansonNote.cpp
@@ -1,7 +1,13 @@
 /*
 For the problem - https://www.hackerrank.com/challenges/ctci-ransom-note/problem
 
-Create a Linked list of all magazine strings
+Create a Double list of all magazine strings
 
 Read every ransom string and if found in list delete the node
 */
+
+class MagazineString{
+  string strData;
+  MagazineString* next;
+  MagazineString* prev;
+};
