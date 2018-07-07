0b60245bd6cd35b8a2dc84a390cdfe866bf340b2
PraGitHub <39001752+PraGitHub@users.noreply.github.com>
2018-07-07 10:19:17 +0530

    Commi_with_manyfixes

--- QuoraAnswerClassifier.cpp
+++ QuoraAnswerClassifier.cpp
@@ -5,15 +5,17 @@
 #include <algorithm>
 using namespace std;
 
-struct PositiveList{
+class PositiveList{
+    public:
     double* dSet;
     PositiveList* next;
-}
+};
 
-struct NegativeList{
+class NegativeList{
+    public:
     double* dSet;
     NegativeList* next;
-}
+};
 
 double StringToDouble(string strData){
     size_t iSize;
@@ -34,7 +36,7 @@ double FindCorrelation(double* dSetX, double* dSetY, int iLen){
     
     for(int i=0;i<iLen;i++){
         double x = dSetX[i];
-        double y = dSety[i];
+        double y = dSetY[i];
         dSumX += x;
         dSumY += y;
         dSumXY += x*y;
@@ -46,10 +48,43 @@ double FindCorrelation(double* dSetX, double* dSetY, int iLen){
     
     dCorrelation = dNumerator / dDenominator ;
     
+    //cout<<dNumerator<<" "<<dDenominator<<" "<<dCorrelation<<",";
+    
     return dCorrelation;
   
 }
 
+double FindEffectiveCorrelation(PositiveList* head, double* dSet, int iLen){
+    PositiveList* temp = head;
+    int iN = 0;
+    double dCorrelationSum = 0;
+    while(temp){
+        iN++;
+        dCorrelationSum += FindCorrelation(temp->dSet,dSet,iLen);
+        temp = temp->next;
+    }
+    //cout<<iN<<endl;
+    //cout<<dCorrelationSum<<endl;
+    double dEffectiveCorrelation = dCorrelationSum/iN;
+    //cout<<dEffectiveCorrelation<<endl;
+    return dEffectiveCorrelation;
+}
+
+double FindEffectiveCorrelation(NegativeList* head, double* dSet, int iLen){
+    NegativeList* temp = head;
+    int iN = 0;
+    double dCorrelationSum = 0;
+    while(temp){
+        iN++;
+        dCorrelationSum  += FindCorrelation(temp->dSet,dSet,iLen);
+        temp = temp->next;
+    }
+    //cout<<iN<<endl;
+    //cout<<dCorrelationSum<<endl;
+    double dEffectiveCorrelation = dCorrelationSum/iN;
+    //cout<<dEffectiveCorrelation<<endl;
+    return dEffectiveCorrelation;
+}
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
@@ -65,52 +100,70 @@ int main() {
         string strID;
         string strType;
         cin>>strID>>strType;
-        double* dSet = new double(iM);
+        //cout<<strType<<" ";
+        double dSet[iM];
         for(int j=0;j<iM;j++){
             string strData;
             cin>>strData;
-            strData = strData.substr(2);
+            size_t iPos = strData.find(":");
+            strData = strData.substr(iPos+1);
+            //cout<<strData<<endl;
             dSet[j] = StringToDouble(strData);
+            //cout<<dSet[j]<<endl;
         }
         if(strType == "+1"){
-            if(pHead == NULL){
-                pHead = new PositiveList();
-                pHead->dSet = dSet;
-                pHead->next = NULL;
-                pCurrent = pHead;
-                continue;
-            }
             PositiveList* pTemp = new PositiveList();
             pTemp->dSet = dSet;
             pTemp->next = NULL;
-            pCurrent->next = pTemp;
-        }
-        if(strType == "-1"){
-            if(nHead == NULL){
-                nHead = new NegativeList();
-                nHead->dSet = dSet;
-                nHead->next = NULL;
-                nCurrent = nHead;
+            if(pHead == NULL){
+                pHead = pTemp;
+                pCurrent = pTemp;
                 continue;
             }
+            pCurrent->next = pTemp;
+            pCurrent = pTemp;
+        }
+        if(strType == "-1"){
             NegativeList* nTemp = new NegativeList();
             nTemp->dSet = dSet;
             nTemp->next = NULL;
+            if(nHead == NULL){
+                nHead = nTemp;
+                nCurrent = nTemp;
+            }
             nCurrent->next = nTemp;
+            nCurrent = nTemp;
         }
     }
     int iQ;
     cin>>iQ;
+    //cout<<iQ;
     for(int i=0;i<iQ;i++){
         string strID;
         cin>>strID;
-        double* dSet = new double(iM);
+        double dSet[iM];
         for(int j=0;j<iM;j++){
             string strData;
-            strData = strData.substr(2);
+            cin>>strData;
+            size_t iPos = strData.find(":");
+            strData = strData.substr(iPos+1);
             dSet[j] = StringToDouble(strData);
+            //cout<<dSet[j]<<" ";
         }
+        double dCorrelationOfPositiveQuery = 0;
+        double dCorrelationOfNegativeQuery = 0;
         
+        dCorrelationOfPositiveQuery = FindEffectiveCorrelation(pHead,dSet,iM);
+        dCorrelationOfNegativeQuery = FindEffectiveCorrelation(nHead,dSet,iM);
+        
+        //cout<<dCorrelationOfPositiveQuery<<" "<<dCorrelationOfNegativeQuery<<endl;
+        
+        if(dCorrelationOfPositiveQuery>dCorrelationOfNegativeQuery){
+            cout<<strID<<" +1"<<endl;
+        }
+        else{
+            cout<<strID<<" -1"<<endl;
+        }
     }
     return 0;
 }
