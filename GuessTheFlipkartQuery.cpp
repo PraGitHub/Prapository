9724a57c234d86dbd32e75eb592fdfdaa0ff23cd
Prashanth H N <39001752+PraGitHub@users.noreply.github.com>
2018-08-03 14:20:38 +0530

    .

--- GuessTheFlipkartQuery.cpp
+++ GuessTheFlipkartQuery.cpp
@@ -33,19 +33,56 @@ string strArrayOfQueries[] = {
     "EOA"
 };
 
+int IsThere(string strLine,string strQuery){
+    if(strLine.find(strQuery)!=strLine.npos){
+        return 1;
+    }
+    return 0;
+}
+
+void strToLower(string &strIn){
+    char* ctemp = (char*)strIn.c_str();
+    while(*ctemp!=0){
+        //cout<<*ctemp<<endl;
+        if(*ctemp >= 'A' && *ctemp <= 'Z'){
+            *ctemp = *ctemp - 'A' + 'a';
+        }
+        ctemp++;
+    }
+}
 
 string GetTheBestMatch(string strLine){
-    string strOutput = "";
     int i=0;
-    int iCount = 0;
+    int iCount = 0;//this varibale is not requires
     string strBestQuery = "";
     while(true){
         string strQuery = strArrayOfQueries[i];
+        string strQueryCopy = strQuery;
         if(strQuery == "EOA"){
             break;
         }
-        
+        int iCountSpaces = 0;
+        int iCountOfThisQuery = 0;
+        size_t iPos = -1;
+        while((iPos = strQuery.find(" "))!=strQuery.npos){
+            iCountSpaces++;
+            string strQuerySubString = strQuery.substr(0,iPos);
+            iCountOfThisQuery += IsThere(strLine,strQuerySubString);
+            strQuery = strQuery.substr(iPos+1);
+        }
+        iCountOfThisQuery += IsThere(strLine,strQuery);
+        if(iCountSpaces+1 == iCountOfThisQuery){
+            strBestQuery = strQueryCopy;
+            break;
+        }
+        //cout<<strBestQuery<<" "<<iCountOfThisQuery<<endl;
+        i++;
+        if(iCountOfThisQuery>iCount){
+            iCount = iCountOfThisQuery;
+            strBestQuery = strQueryCopy;
+        }
     }
+    return strBestQuery;
 }
 
 
@@ -60,6 +97,9 @@ int main() {
             continue;
         }
         //cout<<strLine<<endl;
+        strToLower(strLine);
+        string strQuery = GetTheBestMatch(strLine);
+        cout<<strQuery<<endl;
     }
     return 0;
 }
