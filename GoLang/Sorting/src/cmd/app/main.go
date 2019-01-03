3044cb34c495ca3ac36d3560c6281c98877751f3
Prashanth H N <Prashanth.HN@efi.com>
2019-01-03 11:52:33 +0530

    .

--- GoLang/Sorting/src/cmd/app/main.go
+++ GoLang/Sorting/src/cmd/app/main.go
@@ -9,14 +9,22 @@ func main(){
 	fmt.Println("Sorting app started")
 
 	n := 0
-	arr := make([]int,0)
+	x := 0
+	var arr []int = nil
 	
 	fmt.Println("Enter n = ")
 	fmt.Scanf("%d",&n)
+	fmt.Println("Enter numbers : ")
 
-	for i:=0 ; i < n ; i++{
-		
+	for i:=0 ; i <= n ; i++{
+		fmt.Scanf("%d",&x)
+		if i == 0 {
+			continue;
+		}
+		arr = append(arr,x)
 	}
+
+	fmt.Println("Given array = ",arr)
 
 	Methods.MergeSort();
 	Methods.QuickSort();
