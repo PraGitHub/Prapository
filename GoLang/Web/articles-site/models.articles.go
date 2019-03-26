e11cf712adfe64ce4eb25e7046641f624f735844
prashanh <prashanth.hn@efi.com>
2019-03-26 10:13:41 +0530

    golang-articles-site-addedGETrouteforarticles-added-viewArticlehtml

--- GoLang/Web/articles-site/models.articles.go
+++ GoLang/Web/articles-site/models.articles.go
@@ -1,8 +1,10 @@
 package main
 
 type article struct {
-	ID      int
+	ID      string
+	Author  string
 	Title   string
+	Genre   string
 	Content string
 }
 
@@ -10,11 +12,25 @@ type article struct {
 // In a real application, this list will most likely be fetched
 // from a database or from static files
 var articleList = []article{
-	article{ID: 1, Title: "Article 1", Content: "Article 1 body"},
-	article{ID: 2, Title: "Article 2", Content: "Article 2 body"},
+	article{ID: "1", Author: "Prashanth_1", Title: "Article 1", Genre: "Politics", Content: "Article 1 body"},
+	article{ID: "2", Author: "Prashanth_2", Title: "Article 2", Genre: "Tech", Content: "Article 2 body"},
+	article{ID: "3", Author: "Prashanth_3", Title: "Article 4", Genre: "Movies", Content: "Article 3 body"},
+	article{ID: "4", Author: "Prashanth_4", Title: "Article 4", Genre: "Philosophy", Content: "Article 4 body"},
 }
 
 // Return a list of all the articles
 func getAllArticles() []article {
 	return articleList
+}
+
+func getArticleById(id string) (found bool, retArticle article) {
+	found = false
+	for _, xArticle := range articleList {
+		if xArticle.ID == id {
+			found = true
+			retArticle = xArticle
+			return
+		}
+	}
+	return found, article{"", "", "", "", ""}
 }
--- GoLang/Web/articles-site/router.go
+++ GoLang/Web/articles-site/router.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -17,6 +18,28 @@ func homePage(c *gin.Context) {
 	)
 }
 
+func viewArticle(c *gin.Context) {
+	id := c.Param("id")
+	found, retArticle := getArticleById(id)
+	statusCode := http.StatusNotFound
+	if found {
+		statusCode = http.StatusOK
+	}
+
+	log.Println("viewArticle :: retArticle = ", retArticle)
+
+	c.HTML(
+		statusCode,
+		"view_article.html",
+		gin.H{
+			"title":   retArticle.Title,
+			"found":   found,
+			"payload": retArticle,
+		},
+	)
+}
+
 func initializeRoutes() {
 	router.GET("/", homePage)
+	router.GET("/article/view/:id", viewArticle)
 }
