49a3da9bdc767a2a8764b292fb5619ca3fc871e8
prashanh <prashanth.hn@efi.com>
2019-04-02 09:59:29 +0530

    electron-loadDb-onstrat

--- NodeJS/electron/tutorial/app/mongo.js
+++ NodeJS/electron/tutorial/app/mongo.js
@@ -1,36 +1,58 @@
-let MongoClient = require('mongodb').MongoClient;
-let url = "mongodb://localhost:27017/";
-let $ = require('jquery')  
+
+var MongoClient = require('mongodb').MongoClient;
+var Db = require('mongodb').Db;
+var Server = require('mongodb').Server;
+
 
 function getData(){
-    MongoClient.connect(url,  {useNewUrlParser: true}, function(err, db) {
-    if (err) throw err;
-    let dbo = db.db("CodeReviewSystem");
-    dbo.collection("Users").find({}).toArray(function(err, result) {
-            if (err) throw err;
-            for (let i=0;i<result.length;i++){
-                addUserToList(result[i].username)
-            // addUserToList1(result[i].username) // to test
+    var db = new Db('test', new Server('localhost', 27017));
+    // Establish connection to db
+    db.open(function(err, db) {
+
+    // Use the admin database for the operation
+    var adminDb = db.admin();
+
+    // List all the available databases
+    adminDb.listDatabases(function(err, result) {
+            for(var i=0;i<result.databases.length;i++){
+                addDbNames(result.databases[i]) // to test
+                //assDbNamesToList(result.databases[i])
             }
-            $('#status').text("done fetching")
             db.close();
         });
-    }); 
+    });
 }
 
-$('#loadDb').on('click', () => {
-    // $('#status').text("calling getData");
-     getData(); 
-     //$('#status').text("called getData");
-  }) 
-
 //to test
-function addUserToList1(name){
+function addDbNames(name){
     console.log(name)
 }
 
-function addUserToList(userName) {
+function addDbNamesToList(db) {
+    name = db.name;
+    size = db.sizeOnDisk + 'bytes';
    $('#status').text("fetching")
-    $('#listOfUsers').append('<li><div><alert class="alert alert-info">'+userName +'</alert></div></li><br>')
+    $('#listOfDbs').append('<li><div><alert class="alert alert-info">'+name+' ; '+size +'</alert></div></li><br>')
 }
 
+function mongooseData(){
+    var mongoose = require('mongoose')
+    , Admin = mongoose.mongo.Admin;
+
+    /// create a connection to the DB    
+    var connection = mongoose.createConnection(
+        'mongodb://localhost:27017/admin',{useNewUrlParser: true});
+    connection.on('open', function() {
+        // connection established
+        var adminDb = new Admin(connection.db);
+        adminDb.listDatabases(function(err, result) {
+            console.log('listDatabases succeeded');
+            // database list stored in result.databases
+            var allDatabases = result.databases;    
+            //console.log(allDatabases); to test
+            for (var i=0;i<allDatabases.length;i++){
+                addDbNamesToList(allDatabases[i]);
+            }
+        });
+    });
+}
--- NodeJS/electron/tutorial/app/view.js
+++ NodeJS/electron/tutorial/app/view.js
@@ -11,43 +11,58 @@ $('#minusButton').on('click', () => {
     $('#click-counter').text(count)
  }) 
 
- /*
+ mongooseData();
 
- $('#loadDb').on('click', () => {
+
+ $('#loadCollection').on('click', () => {
     // $('#status').text("calling getData");
-     getData(); 
+    
      //$('#status').text("called getData");
   }) 
 
  //////////////////////////////////////////////////////////////////////////////
 
-let MongoClient = require('mongodb').MongoClient;
-let url = "mongodb://localhost:27017/";
+ function getDbDetails(dbName){
 
-function getData(){
-    MongoClient.connect(url,  {useNewUrlParser: true}, function(err, db) {
-    if (err) throw err;
-    let dbo = db.db("CodeReviewSystem");
-    dbo.collection("Users").find({}).toArray(function(err, result) {
-            if (err) throw err;
-            for (let i=0;i<result.length;i++){
-                addUserToList(result[i].username)
-            // addUserToList1(result[i].username) // to test
+ }
+
+ function addDbNamesToList(db) {
+    name = db.name;
+    size = db.sizeOnDisk +' bytes';
+        $('#listOfDbs').append(`<li class="list-group-item">
+            <div class="row">
+                <div class = "col">
+                    <button onclick="getDbDetails(`+name+`)" class="btn btn-info">`+name+`</button>`+
+                `</div>`+
+                `<div class = "col">
+                    <span class="badge badge-pill badge-dark">`+size+`</span>`+
+                `</div>
+            </div>
+        </li>
+    <br>`)
+}
+
+function mongooseData(){
+    var mongoose = require('mongoose')
+    , Admin = mongoose.mongo.Admin;
+
+    /// create a connection to the DB    
+    var connection = mongoose.createConnection(
+        'mongodb://localhost:27017/admin',{useNewUrlParser: true});
+    connection.on('open', function() {
+        // connection established
+        var adminDb = new Admin(connection.db);
+        adminDb.listDatabases(function(err, result) {
+            console.log('listDatabases succeeded');
+            // database list stored in result.databases
+            var allDatabases = result.databases;    
+            //console.log(allDatabases); to test
+            for (var i=0;i<allDatabases.length;i++){
+                addDbNamesToList(allDatabases[i]);
+                $('#status').text("fetching")
             }
             $('#status').text("done fetching")
-            db.close();
         });
-    }); 
+    });
 }
 
-//to test
-function addUserToList1(name){
-    console.log(name)
-}
-
-function addUserToList(userName) {
-   $('#status').text("fetching")
-    $('#listOfUsers').append('<li><div><alert class="alert alert-info">'+userName +'</alert></div></li><br>')
-}
-
-*/
