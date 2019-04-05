cd5679b6b0c8f3e125212b21b9b307ea1393997b
prashanh <prashanth.hn@efi.com>
2019-04-05 16:02:06 +0530

    cpp-circular-added methods to flight and services classes

--- C_Cpp/circularDependency/circular/circular/Flight.cpp
+++ C_Cpp/circularDependency/circular/circular/Flight.cpp
@@ -2,30 +2,36 @@
 
 Flight::Flight()
 {
-
+	m_pService = nullptr;
+	m_strName = "FLIGHT";
 }
 
 Flight::Flight(string strName)
 {
-
+	m_pService = nullptr;
+	m_strName = strName;
 }
 
 void Flight::SetName(string strName)
 {
-
+	m_strName = strName;
 }
 
 string Flight::GetName()
 {
-
+	return m_strName;
 }
 
 void Flight::SetService(Service* pService)
 {
-
+	m_pService = pService;
 }
 
 void Flight::PrintDetails()
 {
-
+	cout << "Flight :: PrintDetails :: Flight name = " << m_strName.c_str() << endl;
+	if (m_pService)
+	{
+		cout << "Flight :: PrintDetails :: Service name = " << m_pService->GetName().c_str()<<endl;
+	}
 }
--- C_Cpp/circularDependency/circular/circular/Service.cpp
+++ C_Cpp/circularDependency/circular/circular/Service.cpp
@@ -2,29 +2,33 @@
 
 Service::Service()
 {
-
+	m_strName = "SERVICE";
 }
 
 Service::Service(string strName)
 {
-
-}
-
-void Service::AddFlight(Flight* pFlight)
-{
-
-}
-void Service::RemoveFlight(Flight* pFlight)
-{
-
+	m_strName = strName;
 }
 
 string Service::GetName()
 {
+	return m_strName;
+}
 
+void Service::AddFlight(Flight* pFlight)
+{
+	if (pFlight)
+	{
+		m_Flights.push_back(pFlight);
+	}
+}
+
+void Service::RemoveFlight(Flight* pFlight)
+{
+	
 }
 
 void Service::PrintDetails()
 {
-
+	cout << "Service::PrintDetails :: Service name = " << m_strName.c_str() << endl;
 }
