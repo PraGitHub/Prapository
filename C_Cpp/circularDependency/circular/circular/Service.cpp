e844576aa1ce963ed73972ef6dee962a4155b1dd
prashanh <prashanth.hn@efi.com>
2019-04-05 16:12:31 +0530

    cpp-circular-added methods to flight and services classes

--- C_Cpp/circularDependency/circular/circular/Service.cpp
+++ C_Cpp/circularDependency/circular/circular/Service.cpp
@@ -25,10 +25,30 @@ void Service::AddFlight(Flight* pFlight)
 
 void Service::RemoveFlight(Flight* pFlight)
 {
-	
+	if (m_Flights.size() <= 0)
+	{
+		return;
+	}
+	for (vector<Flight*>::iterator iter = m_Flights.begin(); iter != m_Flights.end(); ++iter)
+	{
+		if ((*iter)->GetName() == pFlight->GetName())
+		{
+			m_Flights.erase(iter);
+			break;
+		}
+	}
 }
 
 void Service::PrintDetails()
 {
 	cout << "Service::PrintDetails :: Service name = " << m_strName.c_str() << endl;
+	if (m_Flights.size() <= 0)
+	{
+		return;
+	}
+	for (vector<Flight*>::iterator iter = m_Flights.begin(); iter != m_Flights.end(); ++iter)
+	{
+		Flight* pFlight = *iter;
+		cout << "\tService::PrintDetails :: Flight name = " << pFlight->GetName().c_str() << endl;
+	}
 }
