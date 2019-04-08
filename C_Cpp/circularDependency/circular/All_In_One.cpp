03d3b623bf2abf325d65f0eef84ed40e0680c84b
Prashanth H N <39001752+PraGitHub@users.noreply.github.com>
2019-04-08 11:51:41 +0530

    Update All_In_One.cpp

--- C_Cpp/circularDependency/circular/All_In_One.cpp
+++ C_Cpp/circularDependency/circular/All_In_One.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+/*
 class A;
 class B;
 
@@ -15,7 +16,7 @@ class A{
     }
     void print(){
         cout<<name.c_str()<<endl;
-        b.print();
+        b->print();
     }
 };
 
@@ -23,12 +24,12 @@ class B{
     string name;
     A* a;
     B(){
-        name = "Class B"<<endl;
+        name = "Class B";
         a = new A();
     }
     void print(){
         cout<<name.c_str()<<endl;
-        a.print();
+        a->print();
     }
 }
 
@@ -41,7 +42,67 @@ int main()
     pA->print();
     
     cout<<"Calling A's print "<<endl;
-    pB.print();
+    pB->print();
+    
+    return 0;
+}
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class A;
+class B;
+
+class A{
+    string name;
+    B* b;
+    public:
+    A();
+    void print();
+    
+};
+
+class B{
+    string name;
+    A* a;
+    public:
+    B();
+    void print();
+};
+
+
+A :: A(){
+        name = "Class A";
+        b = new B();
+    }
+    
+void A :: print(){
+        cout<<name.c_str()<<endl;
+        b->print();
+    }
+
+
+ B :: B(){
+        name = "Class B";
+        a = new A();
+    }
+void B :: print(){
+        cout<<name.c_str()<<endl;
+        a->print();
+    }
+
+int main()
+{
+    A* pA = new A();
+    B* pB = new B();
+    
+    cout<<"Calling A's print "<<endl;
+    pA->print();
+    
+    cout<<"Calling A's print "<<endl;
+    pB->print();
     
     return 0;
 }
