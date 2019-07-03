c06373555722b9424323a8525b29f5a692da30da
Prashanth H N <39001752+PraGitHub@users.noreply.github.com>
2019-07-03 07:52:47 +0530

    worst and best case time for passwords

--- Misc/passwordSamsungTest/source.cpp
+++ Misc/passwordSamsungTest/source.cpp
@@ -20,19 +20,41 @@ bool compareElements(string a, string b) //bool compareElements(const string& a,
 
 void determineTheBestAndTheWorst(int n, int k, STRVEC passwords)
 {
-    string password = passwords[n];
-    /*
+/*
     Algorithm:
-    Sort passwords based on length => passwords
+    Sort passwords based on length => passwords // sorting is not required
     LEN => length of actual password
     N1 => number of passwords with their lengths less than LEN
     N2 => number of passwords with their lengths equal to LEN
     BestCaseTime = (N1- (floor(N1/K))) + (floor(N1/K))*5 + 1
     N3 = N1+N2-1
     WorstCasetime = (N3- (floor(N3/K))) + (floor(N3/K))*5 + 1
-    */
-    sort(passwords.begin(), (passwords.begin() + n), compareElements);
-    printStrVec(passwords);
+*/
+    string password = passwords[n];
+    int len = password.length();
+    int n1 = 0, n2 = 0, n3 = 0;
+    int timeBest = -1, timeWorst = -1;
+    //sort(passwords.begin(), (passwords.begin() + n), compareElements); // sorting is not required
+    //printStrVec(passwords);
+    for(int i = 0; i<n; i++)
+    {
+        int thisLen = passwords[i].length();
+        if(thisLen < len)
+        {
+            n1++;
+        }
+        else if(thisLen == len)
+        {
+            n2++;
+        }
+        else;
+    }
+    timeBest = (n1- (n1/k)) + (n1/k)*5 + 1;
+    n3 = n1 + n2 - 1;
+    timeWorst = (n3- (n3/k)) + (n3/k)*5 + 1;
+    
+    cout<<"Best Case Time = "<<timeBest<<endl;
+    cout<<"Worst Case Time = "<<timeWorst<<endl;
 }
 
 int main()
@@ -51,7 +73,7 @@ int main()
         passwords.push_back(strTemp);
     }
 
-    printStrVec(passwords);
+    //printStrVec(passwords);
 
     determineTheBestAndTheWorst(n, k, passwords);
 }
