b761d297f457952d03e92b90d1dea3528b8f6ebd
PraGitHub <39001752+PraGitHub@users.noreply.github.com>
2022-01-30 19:42:56 +0530

    ktor tutorial - 2

--- kotlin/ktor-tutorial/src/main/kotlin/com/example/Application.kt
+++ kotlin/ktor-tutorial/src/main/kotlin/com/example/Application.kt
@@ -12,26 +12,17 @@ import io.ktor.request.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "localhost") {
-        // configureRouting() // We are importing Routing.kt here with import com.example.plugins.* and it has definition of configureRouting function which is tied to Application class
         install(Routing) {
             homeRoute()
-            anyRoute()
         }
+        configureRouting()  // We are importing Routing.kt here with import com.example.plugins.* and,
+                            // it has definition of configureRouting function which is tied to Application class
     }.start(wait = true)
 }
 
 fun Routing.homeRoute() {
     get("/") {
-        println(call.request.queryParameters)
-        println(call.parameters)
-        call.respond("Welcome to Ktor!")
-    }
-}
-
-fun Routing.anyRoute() {
-    get("/{param}") {
-        println(call.request.queryParameters)
-        println(call.parameters)
-        call.respond("Welcome to Ktor!")
+        println("homeRoute GET")
+        call.respond("Welcome to Ktor! Try to GET /home \uD83D\uDE03")
     }
 }
--- kotlin/ktor-tutorial/src/main/kotlin/com/example/plugins/Routing.kt
+++ kotlin/ktor-tutorial/src/main/kotlin/com/example/plugins/Routing.kt
@@ -8,10 +8,35 @@ import io.ktor.request.*
 
 // This function configureRouting belongs to the class Application and hence it can be used directly in the Application.kt file
 fun Application.configureRouting() {
+    val cars = mapOf(
+        Pair("Volkswagen", "Germany"),
+        Pair("Ford", "USA"),
+        Pair("Hyundai", "South Korea"),
+        Pair("Toyota", "Japan"),
+        Pair("Fiat", "Italy")
+    )
 
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        get("/home") {
+            println("configureRouting /home GET")
+            var res = "<h2> Select any one of them \uD83D\uDE03 </h2>"
+            for (key in cars.keys) {
+                res = res + "<ul> <li> <form action=\"/home/${key}\"> <input type=\"submit\" value=\"${key}\"> </form> </li> </ul>"
+            }
+            call.respondText(res, ContentType.Text.Html)
+        }
+
+        get("/home/{car}") {
+            val car = call.parameters.get("car")
+            println("configureRouting /home GET: car = ${car}")
+            val country = cars.get(car) ?: ""
+            if (country == "") {
+                call.respond(HttpStatusCode.BadRequest, "Bad Request. Passed parameter '${car}' is invalid.")
+            } else {
+                var res = "<h2> ${car} is from ${country} \uD83D\uDE03 </h2>"
+                res = res + "<form action=\"/home\"> <input type=\"submit\" value=\"Home\"> </form>"
+                call.respondText(res, ContentType.Text.Html)
+            }
         }
     }
 }
